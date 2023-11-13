1c9f2e82526337dab49976bceda28b3d74ff8dab
ErvinPiscoran <ervin.florin@yahoo.com>
2023-11-13 23:06:41 +0200

    ---

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,44 +2,6 @@ package com.company;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.*;
-
-class studenti{
-    private String nume;
-    private int nota1;
-    private int nota2;
-    private int nota3;
-    public String getNume(){
-        return nume;
-    }
-
-    public void setNume(String nume) {
-        this.nume = nume;
-    }
-
-    public int getNota1() {
-        return nota1;
-    }
-
-    public void setNota1(int nota1) {
-        this.nota1 = nota1;
-    }
-
-    public int getNota2() {
-        return nota2;
-    }
-
-    public void setNota2(int nota2) {
-        this.nota2 = nota2;
-    }
-
-    public int getNota3() {
-        return nota3;
-    }
-
-    public void setNota3(int nota3) {
-        this.nota3 = nota3;
-    }
-}
 public class Main {
     public static void p1(){
         ArrayList<String> Studenti= new ArrayList<String>();
